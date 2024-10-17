7a3e75f8c34987b31a71c16f42323686102a3d8b
Anapp14 <sdmuhngijon4@gmail.com>
2024-10-17 13:25:10 +0700

    six

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,14 +1,7 @@
-import { useState } from "react";
 import Navbar from "./navbar";
 import GameList from "./GameList";
 
 const MainPage = () => {
-  const games = [
-    { id: 1, name: "Mobile Legends", image: "/list-ml.png", description: "Joki rank Mobile Legends." },
-    { id: 2, name: "PUBG Mobile", image: "/list-ml.png", description: "Joki rank PUBG Mobile." },
-    // ... (other games)
-  ];
-
   return (
     <div>
       <Navbar /> {/* Kirim games ke Navbar */}
